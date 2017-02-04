4e33315b2c289454c3810abe29ab6caa4d29f0eb
vogtn <nicolasjvogt@gmail.com>
2017-02-03 16:25:07 -0800

    adding final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+
+//on submit activate this function
+function handleConvert() {
+  var temp = document.getElementById('temp').value;
+  var Faren = document.getElementById('F');
+
+
+  if(Faren.checked == true){
+    convertFaren();
+  }else{
+    convertCels();
+  }
+
+  function convertFaren(){
+    var converted = ((temp-32) * (5/9));
+    document.getElementById('newConvert').innerHTML = converted + " Degrees Celcius";
+  }
+  function convertCels(){
+    var converted = ((temp * 9/5) +(32));
+    document.getElementById('newConvert').innerHTML = converted + " Degrees Farenheit";
+  }
+
+}
